041c47c67db8f4f69719d4a0d5efa1aa9d74b929
Aki-Ito <60352606+Aki-Ito@users.noreply.github.com>
2022-06-13 23:41:44 +0900

    generally finished

--- KeyChainAccess/ViewController.swift
+++ KeyChainAccess/ViewController.swift
@@ -9,25 +9,41 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    let id: String = "id"
+    let key: String = "key"
+    var password = String()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        switch load(id: self.id, key: self.key){
+        case true:
+            textField.text = password
+            break
+        case false:
+            break
+        }
     }
     
     
-    func save<T: Codable>(id: String, key: String, data: T){
-        let encoder = JSONEncoder()
-        do{
-            let encoded = try encoder.encode(data)
-            
+    @IBAction func addKeyChain(_ sender: Any) {
+        save(id: self.id, key: self.key)
+    }
+    func save(id: String, key: String){
+            let str: String = textField.text!
+            let data = str.data(using: .utf8)
+
+        
             let query: [String: Any] = [
                 //一般的なパスワード
                 kSecClass as String: kSecClassGenericPassword,
                 //一位性を担保する
                 kSecAttrService as String: key,
+                //一位性を保持する
                 kSecAttrAccount as String: id,
-                kSecValueData as String: encoded
+                //保存するデータ
+                kSecValueData as String: data as Any
             ]
             
             //データの存在を確認する
@@ -35,22 +51,18 @@ class ViewController: UIViewController {
             
             //データの有無によって挙動を変える。
             switch status {
+                //何もない場合追加する
             case errSecItemNotFound:
                 SecItemAdd(query as CFDictionary, nil)
-                
+                //存在する場合アプデをかける
             case errSecSuccess:
-                SecItemUpdate(query as CFDictionary, [kSecValueData as String: encoded] as CFDictionary)
-                
+                SecItemUpdate(query as CFDictionary, [kSecValueData as String: textField.text] as CFDictionary)
             default:
                 print("none")
             }
-            
-        }catch{
-            print("error")
-        }
     }
     
-    func load<T: Codable>(id: String, key: String, type: T.Type) -> T?{
+    func load(id: String, key: String) -> Bool{
         let query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrService as String: key,
@@ -64,23 +76,19 @@ class ViewController: UIViewController {
         
         switch status {
         case errSecItemNotFound:
-            return nil
+            return false
         case errSecSuccess:
             guard let item = item,
                   let value = item[kSecValueData as String] as? Data else {
                       print("no data")
-                      return nil
+                      return false
                   }
-            do {
-                return try JSONDecoder().decode(type, from: value)
-            }catch{
-                print("error")
-            }
+            password = String(data: value, encoding: .utf8)!
+            return true
         default:
             print("no data conformed")
+            return false
         }
-        
-        return nil
     }
 
 
